ef9398e10479dc5edd29ef71e0763734293f611d
Matteus Isaque <matteus.isaque28@gmail.com>
2023-02-24 12:03:28 -0300

    alte

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Flex, Input, Stack, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Text, Flex, Input, Stack, Grid, GridItem, useMediaQuery } from '@chakra-ui/react'
 import ButtonCompoent from './components/button'
 
 import TextAndButton from './components/textAndIcon'
@@ -6,12 +6,33 @@ import Image from 'next/image'
 
 import Logo from './img/ami.jpg'
 import Dowload from './img/download.jpg'
+import mobile from './img/mobile.jpeg'
 import Capa from './img/capa.jpg'
 
 
 import Link from 'next/link'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function Index() {
+  const [isLargerThan768] = useMediaQuery('(min-width: 768px)')
+  const [name, setName] = useState<String>()
+  const [number, setNumber] = useState<Number>()
+  const [email, setEmail] = useState<String>()
+
+
+
+  async function handleForm(){
+    axios.post(`${process.env.FORM_LINK}/ticonnected`, {
+      emailTo: "matteus.isaque28@gmail.com",
+      title: "campanha isaques estúdios",
+      domain:"https://site.com.br",
+      name,
+      phone: number,
+      email
+    })
+
+  }
 
   return (
     <Box as='main'>
@@ -25,16 +46,16 @@ export default function Index() {
       </Flex>
 
       <Grid id='form'
-      style={{backgroundImage:`url(${Capa.src})`}}
-      gridTemplateColumns={{ base: '1fr', lg: '1fr 1fr' }} px='10%' py={{ base: '100px', md: '200px' }}>
+        style={{ backgroundImage: `url(${Capa.src})` }}
+        gridTemplateColumns={{ base: '1fr', lg: '1fr 1fr' }} px='10%' py={{ base: '100px', md: '200px' }}>
         <GridItem py={{ base: '40px', sm: '60px', lg: '75px' }} px={{ base: '20px', sm: '40px', lg: '75px' }} bgColor='teal.500'>
-          <Stack as='form' spacing='4' textAlign='center' color='teal.500'>
+          <Stack as='form' onSubmit={handleForm} spacing='4' textAlign='center' color='teal.500'>
             <Text fontSize='24px' fontWeight='800' color='white'>PLANOS A PARTIR DE 1,00$ POR DIA</Text>
             <Text color='white'>Contrate agora e comece a usar em 24H</Text>
-            <Input placeholder='nome' type='text' bgColor='white' />
-            <Input placeholder='numero' type='number' bgColor='white' />
-            <Input placeholder='email' type='email' bgColor='white' />
-            <ButtonCompoent bgColor='white' color='teal.500' color_hover='white' text='enviar' />
+            <Input placeholder='nome' type='text' bgColor='white' onChange={(e)=>{setName(e.target.value)}} />
+            <Input placeholder='numero' type='number' bgColor='white' onChange={(e)=>{setNumber(Number(e.target.value))}}/>
+            <Input placeholder='email' type='email' bgColor='white' onChange={(e)=>{setEmail(e.target.value)}}/>
+            <ButtonCompoent bgColor='white' color='teal.500' color_hover='white' text='enviar' type='submit' />
           </Stack>
         </GridItem>
       </Grid>
@@ -88,8 +109,9 @@ export default function Index() {
               <Text fontSize={{ base: '40px', lg: '60px' }} fontWeight={700}>R$ 39,90</Text>
               <Text fontSize='20px'>por mês/vida</Text>
             </Box>
-
-            <ButtonCompoent mt='40px' text='Falar com atendente' />
+            <Link style={{ width: '100%' }} href='https://wa.me/5511961062860'>
+              <ButtonCompoent w='100%' mt='40px' text='Falar com atendente' />
+            </Link>
           </Flex>
 
           <Flex width={{ md: '350px', xl: '450px' }} mt={{ base: '75px', md: '0px' }} flexDirection='column' p='20px' borderStyle='solid' borderWidth='1px' borderColor='teal.500' boxShadow='2xl'>
@@ -109,17 +131,26 @@ export default function Index() {
               <Text fontSize='20px'>por mês/vida</Text>
             </Box>
 
-            <ButtonCompoent mt='40px' text='Falar com atendente' />
+            <Link style={{ width: '100%' }} href='https://wa.me/5511961062860'>
+              <ButtonCompoent w='100%' mt='40px' text='Falar com atendente' />
+            </Link>
           </Flex>
 
         </Flex>
       </Box>
 
-      <Link href='/'>
-        <Box id='end' style={{backgroundImage:`url(${Dowload.src})`}} w='100%' h='350px'>
-        </Box>
-      </Link>
-
+      {
+        isLargerThan768 ?
+          <Link href='/?couter=#form'>
+            <Box id='end' style={{ backgroundImage: `url(${Dowload.src})` }} w='100%' h='350px'>
+            </Box>
+          </Link>
+          :
+          <Link href='/?couter=#form'>
+            <Box id='end' style={{ backgroundImage: `url(${mobile.src})` }} w='100%' h='350px'>
+            </Box>
+          </Link>
+      }
       <Box bgColor='teal.500' w='100%' textAlign='center' color='white' py='2'>
         <Text fontSize='14px'>Todos os direitos reservados aos seus criadores</Text>
       </Box>
