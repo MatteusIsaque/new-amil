b4e023fd71ac1e1291ab138f150d1ec2db3b7b12
Matteus Isaque <matteus.isaque28@gmail.com>
2023-02-27 10:56:05 -0300

    alteração

--- pages/components/button.tsx
+++ pages/components/button.tsx
@@ -9,7 +9,7 @@ interface ButtonPropsComponent extends ButtonProps {
 
 export default function ButtonCompoent({ text, bgColor = 'teal.500', color = 'white', color_hover, ...rest }: ButtonPropsComponent) {
   return (
-    <Button type='submit' bgColor={bgColor} color={color} transition='0.5s' _hover={{ bgColor: 'teal.600', color: color_hover, transition: '0.5s' }} {...rest}>
+    <Button id="button" type='submit' bgColor={bgColor} color={color} transition='0.5s' _hover={{ bgColor: 'teal.600', color: color_hover, transition: '0.5s' }} {...rest}>
       {text}
     </Button>
   )
--- pages/components/zapButton.tsx
+++ pages/components/zapButton.tsx
@@ -7,9 +7,9 @@ import ZapBTN from './../img/wts.png'
 export default function ZapButton() {
 
   return (
-    <Box position='fixed' bottom='10px' right='10px' cursor='pointer' width={70} zIndex='100'>
-      <Link href='https://wa.me/5511961062860'>
-        <Image src={ZapBTN} alt='zap' />
+    <Box id='zap' position='fixed' bottom='10px' right='10px' cursor='pointer' width={70} zIndex='100'>
+      <Link id='zap' href='https://wa.me/5511961062860'>
+        <Image id='zap' src={ZapBTN} alt='zap' />
       </Link>
     </Box>
   )
